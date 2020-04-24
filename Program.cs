995a4704c053e893eac63b160d0449ef9d5cf625
Jeremiah Price <JeremiahP94@outlook.com>
2020-04-24 11:42:00 -0600

    added console.writelines to each demension.

--- Program.cs
+++ Program.cs
@@ -12,17 +12,19 @@ namespace ConsoleApp1
             double width, height, woodLength, glassArea;
             string widthString, heightString;
 
+            Console.WriteLine("Type the width (meters): ");
             widthString = Console.ReadLine();
             width = double.Parse(widthString);
 
+            Console.WriteLine("Tyoe the height (meters): ");
             heightString = Console.ReadLine();
             height = double.Parse(heightString);
 
             woodLength = 2 * (width + height) * 3.25;
             glassArea = 2 * (width * height);
 
-            Console.WriteLine("The length of the wood is " + woodLength + " feet");
-            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+            Console.WriteLine("The length of the wood is " + woodLength + " meters.");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres.");
             Console.WriteLine("My name is " + message + " and I am from " + message1);
 
             Console.ReadKey();
